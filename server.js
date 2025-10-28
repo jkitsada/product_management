87aad7b80862503add045894edb109b8cdc2c2e2
jkitsada <j.kitsada@gmail.com>
2025-10-28 19:26:22 +0700

    message

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { Pool } = require('pg');
 require('dotenv').config();
